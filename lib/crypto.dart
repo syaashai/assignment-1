4d46a96d74ac22e7e3da7bc409b27c7a05f2edd1
syaurah shaibon <84198467+syaashai@users.noreply.github.com>
2022-04-25 17:46:37 +0800

    init

---
+++ lib/crypto.dart
@@ -0,0 +1,141 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:ndialog/ndialog.dart';
+
+void main() {
+  runApp(const BitcoinScreen());
+}
+
+class BitcoinScreen extends StatelessWidget {
+  const BitcoinScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        scaffoldBackgroundColor: Colors.teal[50],
+      ),
+      home: const MyCryptoPage(),
+    );
+  }
+}
+
+class MyCryptoPage extends StatefulWidget {
+  const MyCryptoPage({Key? key}) : super(key: key);
+
+  @override
+  State<MyCryptoPage> createState() => _MyCryptoPageState();
+}
+
+class _MyCryptoPageState extends State<MyCryptoPage> {
+  String select = "aed", description = "", name = "", type = "", unit = "";
+  double value = 0.0;
+  List<String> exList = [
+    "eth",
+    "ltc",
+    "bch",
+    "bnb",
+    "eos",
+    "xrp",
+    "xlm",
+    "dot",
+    "yfi",
+    "usd",
+    "aed",
+    "ars",
+    "aud",
+    "bdt",
+    "bhd",
+    "bmd",
+    "brl",
+    "cad",
+    "chf",
+    "clp",
+    "cny",
+    "czk",
+    "dkk",
+    "eur",
+    "gbp",
+    "hkd",
+    "huf",
+    "idr",
+    "ils",
+    "inr",
+    "jpy",
+    "krw",
+    "kwd",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Bitcoin cryptocurrency ")),
+        body: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: const Text(
+                    "\t\t\t\t\t\t\Select fiat or crypto money to display the value exchange",
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                DropdownButton(
+                  itemHeight: 60,
+                  value: select,
+                  onChanged: (newValue) {
+                    setState(() {
+                      select = newValue.toString();
+                    });
+                  },
+                  items: exList.map((selectLoc) {
+                    return DropdownMenuItem(
+                      child: Text(
+                        selectLoc,
+                      ),
+                      value: selectLoc,
+                    );
+                  }).toList(),
+                ),
+                ElevatedButton(
+                    onPressed: _loadCrypto, child: const Text("load")),
+                const SizedBox(height: 18),
+                Text(
+                  description,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    fontFamily: 'Raleway',
+                  ),
+                ),
+              ],
+            ))));
+  }
+
+  Future<void> _loadCrypto() async {
+    ProgressDialog progressDialog = ProgressDialog(context,
+        message: const Text("progress"), title: const Text("Searching...."));
+    progressDialog..show();
+
+    var url = Uri.parse("https://api.coingecko.com/api/v3/exchange_rates");
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var jsonData = response.body;
+      var parsedData = json.decode(jsonData);
+      name = parsedData['rates'][select]['name'];
+      unit = parsedData['rates'][select]['unit'];
+      value = parsedData['rates'][select]['value'];
+      type = parsedData['rates'][select]['type'];
+
+      setState(() {
+        description =
+            " \n Name: $name \n Unit: $unit \n Value: $value \n Type: $type";
+      });
+      progressDialog.dismiss();
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'crypto.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (content) => BitcoinScreen())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('assets/images/bitcoin.jpg', scale: 2.5),
+      ),
+    );
+  }
+}
